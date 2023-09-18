4c681adea4bc9d60b99b12267f6fe70342ded8cc
Ravichandra-Mulage <111578428+Ravichandra-Mulage@users.noreply.github.com>
2023-09-18 19:13:25 +0000

    Learned about variables.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 12423
+
+let accountEmail = "Ravichandra@gmail.com"
+
+var accountPassword = "12345"
+
+accountCity = "Jaipur"
+
+let accountState;
+// accountId = 2  // not allowed
+
+/*
+
+prefer not use var
+
+because of issue in block scope and functional scope
+
+*/
+
+accountEmail = "ravichandramulage@00gmail.com"
+
+accountPassword = "32423134"
+
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
