d9af6bf50bc6f4996e8abee8a3083e4116e6cb5f
Ravichandra-Mulage <111578428+Ravichandra-Mulage@users.noreply.github.com>
2023-09-18 19:35:38 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+// code readability should be high
+console.log("Ravichandra");
+
+let name = "Ravichandra"
+
+let age = 22
+
+let isLoggedIn = false
+
+
+// All differnt dataTypes
+// number => range (2 to power 53)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol 
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
