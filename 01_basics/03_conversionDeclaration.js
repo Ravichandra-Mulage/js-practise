aa82123a1ccf4cf045ab57da6b4831ee82d1b98f
Ravichandra-Mulage <111578428+Ravichandra-Mulage@users.noreply.github.com>
2023-09-19 14:07:48 +0000

    Learned about datatypes conversion.

---
+++ 01_basics/03_conversionDeclaration.js
@@ -0,0 +1,54 @@
+let score = "33abc"
+
+//  console.log(typeof score);
+//  console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+
+ // console.log(typeof valueInNumber);
+
+ // console.log(valueInNumber);
+
+ let marks = false;
+
+ let changeIntoNumber = Number(marks)
+
+// console.log(changeIntoNumber);
+
+// ********************** ConvertIntoNumbers **********************
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false = 0
+
+// ********************** ConvertIntoBoolean **********************
+
+ let isLoggedIn = ""
+
+ let isBooleanLoggedIn = Boolean(isLoggedIn)
+
+  console.log(isBooleanLoggedIn);
+
+  console.log(typeof isBooleanLoggedIn);
+
+ // 1 => true; 0 => false
+ // "Ravichandra" => true
+ // "" => false
+
+ // ********************** ConvertIntoString **********************
+
+ let someNumber = 33
+
+ let strignNumber = String(someNumber)
+
+ //  console.log(strignNumber);
+
+ // console.log(typeof strignNumber)
+
+
+
+
+
+
+ 
+
