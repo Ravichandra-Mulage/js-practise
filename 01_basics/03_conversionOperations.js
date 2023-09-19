e67a62c31404873145c8f7d5092e96a04554e62b
Ravichandra-Mulage <111578428+Ravichandra-Mulage@users.noreply.github.com>
2023-09-19 21:05:26 +0000

    Learned about various operations in JS.

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,9 +27,9 @@ let score = "33abc"
 
  let isBooleanLoggedIn = Boolean(isLoggedIn)
 
-  console.log(isBooleanLoggedIn);
+  // console.log(isBooleanLoggedIn);
 
-  console.log(typeof isBooleanLoggedIn);
+  // console.log(typeof isBooleanLoggedIn);
 
  // 1 => true; 0 => false
  // "Ravichandra" => true
@@ -44,6 +44,43 @@ let score = "33abc"
  //  console.log(strignNumber);
 
  // console.log(typeof strignNumber)
+
+ // *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
 
 
