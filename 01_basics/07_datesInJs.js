863c173dec2a38dca4e09d6e2fda71d3c6074f30
Ravichandra-Mulage <111578428+Ravichandra-Mulage@users.noreply.github.com>
2023-09-20 18:26:13 +0000

    Understood about date and time in JavaScript.

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,43 @@
+let myDate = new Date ()
+
+// console.log(myDate.toString()); // return  Wed Sep 20 2023 15:00:21 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.toDateString()); // returns Wed Sep 20 2023
+
+// console.log(myDate.toISOString()); // returns 2023-09-20T15:03:00.245Z
+
+// console.log(myDate.toJSON()); // returns 2023-09-20T15:03:54.761Z
+// console.log(myDate.toLocaleString()) // returns 20/9/2023, 3:06:07 pm
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date("01-14-2023");
+
+//  console.log(myCreatedDate);
+
+//  console.log(myCreatedDate.toLocaleString());
+
+// console.log(myCreatedDate.toDateString())
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+
+//  console.log(myCreatedDate.getTime());
+ 
+//  console.log(Math.floor(Date.now()/ 1000));
+
+ let newDate = new Date()
+
+// console.log(newDate);
+//  console.log(newDate.getDay());
+//  console.log(newDate.getDate());
+//  console.log(newDate.getFullYear());
+//  console.log(newDate.getHours());
+
+ // Create a new Date object representing the current date and time
+const currentDate = new Date();
+
+// Get various components of the date and time
+
+console.log(newDate.toLocaleString())
