7a2ce7bc45515b8eeb1fd85ac0336afbf03131ea
Ravichandra-Mulage <111578428+Ravichandra-Mulage@users.noreply.github.com>
2023-09-25 15:30:08 +0000

    understood about if, switch & truthy-falsy.

---
+++ 04_control_flow/one.js
@@ -0,0 +1,44 @@
+// if
+
+/*
+
+if(2 === "2"){
+        console.log("sahi baat hai.");
+} else {
+    console.log("galat baat hai, data types sahi nahi.");
+}
+
+*/
+
+// comparison operators - <, >, <=, >=, ==, !=, ===(it's also checks the type of the variable, aka strict check)
+
+const balance =1000
+
+// if(balance > 500) console.log("test"); // implicit scope
+
+/*
+
+if(balance < 500){
+    console.log("less than 500")
+} else if( balance < 750){
+    console.log("less than 750");
+} else if( balance < 900){
+    console.log("less than 900");
+} else {
+    console.log("less than 1200");
+}
+
+*/
+
+/*
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allowed to buy courses.");
+}
+
+*/
+
+
+
